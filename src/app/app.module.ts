48836513811e950401598df70f506cc46bf35af3
Javier Lete <jglete@gmail.com>
2019-05-29 20:23:55 +0200

    MenuPrincipal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuPrincipalComponent } from './menu-principal/menu-principal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuPrincipalComponent
   ],
   imports: [
     BrowserModule,
