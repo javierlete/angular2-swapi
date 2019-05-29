bb12aaf47bf08255a1ec26d0c65f6504e2559059
Javier Lete <jglete@gmail.com>
2019-05-29 20:28:13 +0200

    Rutado raíz

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MenuPrincipalComponent } from './menu-principal/menu-principal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MenuPrincipalComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
